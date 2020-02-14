c417dc61faecb69311e6bbd823badb66013b6127
olpotkin <olpotkin@gmail.com>
2020-02-14 12:16:47 +0100

    feat: Add player score

---
+++ Cubo Race/Assets/Scripts/Score.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Transform player_pos;
+    public Text scoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(player_pos.position.z);
+        scoreText.text = player_pos.position.z.ToString("0");
+    }
+}
